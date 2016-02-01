3716ca6003d54ca0d2ea9c6f9f6522b60373917e
Dom97 <Dom97@vt.edu>
2016-02-01 11:16:55 -0500

    Prime factor first commit

---
+++ PrimeFactors.Tests/PrimeFactorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeFactors.Tests
+{
+    [TestClass]
+    public class PrimeFactorTests
+    {
+        [TestMethod]
+        public void GenerateOneReturnsEmptyList()
+        {
+            var primeFactors = PrimeFactors.Generate(1);
+            Assert.IsFalse(primeFactors.Any());
+        }
+
+        [TestMethod]
+        public void GenerateTwoReturnsTwo()
+        {
+            var expected = new List<Int32>() { 2 };
+            CollectionAssert.AreEqual(expected, PrimeFactors.Generate(2));
+        }
+
+        [TestMethod]
+        public void GenerateThreeReturnsThree()
+        {
+            var expected = new List<Int32>() { 3 };
+            CollectionAssert.AreEqual(expected, PrimeFactors.Generate(3));
+        }
+
+        [TestMethod]
+        public void GenerateFourReturnsTwoTwo()
+        {
+            var expected = new List<Int32>() { 2, 2 };
+            CollectionAssert.AreEqual(expected, PrimeFactors.Generate(4));
+        }
+
+        [TestMethod]
+        public void GenerateSixReturnsTwoThree()
+        {
+            var expected = new List<Int32>() { 2, 3 };
+            CollectionAssert.AreEqual(expected, PrimeFactors.Generate(6));
+        }
+
+        [TestMethod]
+        public void GenerateEightReturnsTwoTwoTwo()
+        {
+            var expected = new List<Int32>() { 2, 2, 2 };
+            CollectionAssert.AreEqual(expected, PrimeFactors.Generate(8));
+        }
+
+        [TestMethod]
+        public void GenerateNineReturnsThreeThree()
+        {
+            var expected = new List<Int32>() { 3, 3 };
+            CollectionAssert.AreEqual(expected, PrimeFactors.Generate(9));
+        }
+    }
+}
---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public static List<Int32> Generate(Int32 number)
+        {
+            var factors = new List<Int32>();
+
+            for (var i = 2; i <= number; i++)
+            {
+                while (number % i == 0)
+                {
+                    factors.Add(i);
+                    number /= i;
+                   
+                    if (number <= 1)
+                        return factors;
+                }
+            } 
+            
+            return factors;
+        }
+    }
+}
